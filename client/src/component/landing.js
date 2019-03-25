6aa20cff66eaf4d62c6146dcdfb6be3d126a4719
Himanshu Mehta <himanshum2497@gmail.com>
2019-03-25 02:26:16 -0700

    working on landing page

--- client/src/component/landing.js
+++ client/src/component/landing.js
@@ -1,35 +1,15 @@
 import React, { Component } from 'react'
-import LandingImg from '../images/landing.jpg'
 import SimpleCard from './simpleCard.js'
+import Land from '../images/landing.jpg'
 import Logo from '../images/logo.png'
 
 export default class Landing extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { width: 0, height: 0 };
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    }
-        
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener('resize', this.updateWindowDimensions);
-    }
-        
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-    }
-        
-    updateWindowDimensions() {
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
-    }
-
-
   render() {
     return (
     <div>
       <div>
-        <img src={LandingImg} alt="Landing" style={{width: this.state.width, height: "50%", backgroundSize: 'cover', zIndex: 0}}/>
-        <div style={{fontSize: 35, textAlign: 'center', position: 'absolute', top: '30%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+        <img src={Land} alt="Landing" style={{width: "100%", height: "50%", backgroundSize: 'cover', zIndex: 0}}/>
+        <div style={{fontSize: 35, color: "white", textAlign: 'center', position: 'absolute', top: '30%', left: '50%', transform: 'translate(-50%, -50%)'}}>
             <h1>  
                <img src={Logo} alt="logo" style={{ width: 100, height: 100, verticalAlign: "bottom"}} /> 
                Alternative Dojo 2.0

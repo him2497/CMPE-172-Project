3f8053b4fe5c4d57ec1393c8a280c2c821fed6b3
Himanshu Mehta <himanshum2497@gmail.com>
2019-05-06 01:19:32 -0700

    updated the server address

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,9 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/auth/', { target: 'http://localhost:8080', changeOrigin: true }));
-  app.use(proxy('/user/', { target: 'http://localhost:8080', changeOrigin: true }));
-  app.use(proxy('/check/', { target: 'http://localhost:8080', changeOrigin: true }));
-  app.use(proxy('/admin/', { target: 'http://localhost:8080', changeOrigin: true }));
+  app.use(proxy('/auth/', { target: 'http://ec2-54-241-156-232.us-west-1.compute.amazonaws.com:8000/', changeOrigin: true }));
+  app.use(proxy('/user/', { target: 'http://ec2-54-241-156-232.us-west-1.compute.amazonaws.com:8000/', changeOrigin: true }));
+  app.use(proxy('/check/', { target: 'http://ec2-54-241-156-232.us-west-1.compute.amazonaws.com:8000/', changeOrigin: true }));
+  app.use(proxy('/admin/', { target: 'http://ec2-54-241-156-232.us-west-1.compute.amazonaws.com:8000/', changeOrigin: true }));
 
 };

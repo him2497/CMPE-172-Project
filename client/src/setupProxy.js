0eb07ba8e46ca93fefedafbde5ac6c38c5cc5604
Himanshu Mehta <himanshum2497@gmail.com>
2019-05-06 02:26:31 -0700

    added a api url

--- client/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-// const proxy = require('http-proxy-middleware');
-
-// module.exports = function(app) {
-//   app.use(proxy('/auth/', { target: 'http://localhost:8080', changeOrigin: true }));
-//   app.use(proxy('/user/', { target: 'http://localhost:8080', changeOrigin: true }));
-//   app.use(proxy('/check/', { target: 'http://localhost:8080', changeOrigin: true }));
-//   app.use(proxy('/admin/', { target: 'http://localhost:8080', changeOrigin: true }));
-
-// };
